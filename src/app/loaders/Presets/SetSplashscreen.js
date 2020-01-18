ab08c229a32210d96f140e2459324abb67ab3e65
Sominemo <me@sominemo.com>
2020-01-18 03:36:42 +0200

    Splashscreen

---
+++ src/app/loaders/Presets/SetSplashscreen.js
@@ -0,0 +1,7 @@
+import SplashScreenController from "@Environment/Loaders/SplashScreenController"
+
+SplashScreenController.name = "monobank PWA"
+SplashScreenController.splashImage.innerHTML = require("@Resources/images/logo/vector.svg")
+
+SplashScreenController.splashImage = SplashScreenController.splashImage.firstChild
+SplashScreenController.splashImage.style.height = "30vh"
